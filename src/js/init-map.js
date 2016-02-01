310722933d951da495b98c8e4820d8225f856df3
Francisco Aranda <flarandagarcia@gmail.com>
2016-02-01 18:06:47 +0100

    New logos and css changes

--- src/js/init-map.js
+++ src/js/init-map.js
@@ -4,7 +4,7 @@ $( document ).ready( () => {
     let map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/flaranda/cijsj80e80092bfm2q864opbl',
-        center: [-4.782734, 37.890154],
+        center: [-4.7832689, 37.8922254],
         maxZoom: 15,
         minZoom: 15,
         zoom: 15,
